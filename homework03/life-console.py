13552806bd6770a9e914f253c427f5c44546163b
9Anpanman <kondratev-alesha69@mail.ru>
2020-11-12 16:46:37 +0300

    life-consolfe

--- homework03/life-console.py
+++ homework03/life-console.py
@@ -10,13 +10,51 @@ class Console(UI):
 
     def draw_borders(self, screen) -> None:
         """ Отобразить рамку. """
-        pass
+
+        screen.clear()
+        y, x = screen.getmaxyx()
+        string = ""
+
+        for row in range(y):
+            for col in range(x):
+                if row == 0 or row == (y - 1):
+                    if col == 0 or col == x:
+                        string += "*"
+                    else:
+                        string += "-"
+                elif row < (y - 1) and row > 0:
+                    if col == 0  or col == (x - 1):
+                        string += "|"
+                    else:
+                        string += " "
+            try:
+                screen.addstr(string)
+            except curses.error:
+                pass
+            string = ""
+
+        self.draw_grid(screen)
+        screen.refresh()
+        screen.getch()
 
     def draw_grid(self, screen) -> None:
         """ Отобразить состояние клеток. """
-        pass
+
+        y, x = screen.getmaxyx()
+        col = (x - self.life.cols) // 2
+        row = (y - self.life.rows) // 2
+        for count_row, just_row in enumerate(self.life.curr_generation):
+            for count_col, just_col in enumerate(just_row):
+                if just_col:
+                    try:
+                        screen.addstr(count_row + row, count_col + col, "*")
+                    except curses.error:
+                        pass
 
     def run(self) -> None:
         screen = curses.initscr()
-        # PUT YOUR CODE HERE
+        curses.wrapper(self.draw_borders)
+        while not self.life.is_max_generations_exceeded and self.life.is_changing:
+            self.life.step()
+            self.draw_borders(screen)
         curses.endwin()
