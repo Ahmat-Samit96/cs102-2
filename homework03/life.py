1f5a581aef90824a01501e11526f75301c6ae048
9Anpanman <kondratev-alesha69@mail.ru>
2020-11-12 19:44:28 +0300

    black

--- homework03/life.py
+++ homework03/life.py
@@ -11,12 +11,11 @@ Grid = List[Cells]
 
 
 class GameOfLife:
-    
     def __init__(
         self,
         size: Tuple[int, int],
-        randomize: bool=True,
-        max_generations: Optional[float]=float('inf')
+        randomize: bool = True,
+        max_generations: Optional[float] = float("inf"),
     ) -> None:
         # Размер клеточного поля
         self.rows, self.cols = size
@@ -29,7 +28,7 @@ class GameOfLife:
         # Текущее число поколений
         self.generations = 1
 
-    def create_grid(self, randomize: bool=False) -> Grid:
+    def create_grid(self, randomize: bool = False) -> Grid:
 
         grid = []
         if randomize == False:
@@ -42,7 +41,7 @@ class GameOfLife:
 
         neighbours = []
         row, col = cell
-        for i in range(-1,2):
+        for i in range(-1, 2):
             for j in range(-1, 2):
                 if 0 <= row + i < self.cols and 0 <= col + j < self.rows and (i, j) != (0, 0):
                     neighbours.append(self.curr_generation[row + i][col + j])
@@ -55,7 +54,9 @@ class GameOfLife:
             for j in range(self.rows):
                 if (self.curr_generation[i][j] == 0) and sum(self.get_neighbours([i, j])) == 3:
                     copy_grid[i][j] = 1
-                elif (self.curr_generation[i][j] == 1) and (1 < sum(self.get_neighbours([i, j])) < 4):
+                elif (self.curr_generation[i][j] == 1) and (
+                    1 < sum(self.get_neighbours([i, j])) < 4
+                ):
                     copy_grid[i][j] = 1
 
         return copy_grid
@@ -64,7 +65,7 @@ class GameOfLife:
         """
         Выполнить один шаг игры.
         """
-        
+
         self.prev_generation = copy.deepcopy(self.curr_generation)
         self.curr_generation = self.get_next_generation
         self.generations += 1
@@ -77,39 +78,38 @@ class GameOfLife:
         if self.generations <= self.max_generations:
             return True
         else:
-            return  False
+            return False
 
     @property
     def is_changing(self) -> bool:
         """
         Изменилось ли состояние клеток с предыдущего шага.
         """
-        
+
         if self.prev_generation == self.curr_generation:
             return False
         else:
             return True
 
     @staticmethod
-    def from_file(filename: pathlib.Path) -> 'GameOfLife':
+    def from_file(filename: pathlib.Path) -> "GameOfLife":
         """
         Прочитать состояние клеток из указанного файла.
         """
 
         doc = open(filename, "r")
-        doc_grid = [[int(col) for col in row.strip()] for row in doc ]
+        doc_grid = [[int(col) for col in row.strip()] for row in doc]
         doc.close()
 
         game = GameOfLife((len(doc_grid), len(doc_grid[0])))
         game.curr_generation = doc_grid
         return game
-    
 
     def save(self, filename: pathlib.Path) -> None:
         """
         Сохранить текущее состояние клеток в указанный файл.
         """
-        
+
         doc = open(filename, "w")
         for row in self.curr_generation:
             for col in row:
