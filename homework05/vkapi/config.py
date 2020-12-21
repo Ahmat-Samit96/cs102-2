75199aada32b7cc6a9cb2ae6bec4512eb57e3d5b
9Anpanman <kondratev-alesha69@mail.ru>
2020-12-21 19:22:43 +0300

    access_token

--- homework05/vkapi/config.py
+++ homework05/vkapi/config.py
@@ -2,6 +2,6 @@
 
 VK_CONFIG = {
     "domain": "https://api.vk.com/method",
-    "access_token": "",
+    "access_token": "38184bca142ec3be22bbdc4d3d02d88d466f346847d1783f8d8a8a3606bc8babe532f9e8524cbe5bf1a4f",
     "version": "5.126",
 }
