220504ec17567d6536f7ae65a2ef7428686767d7
9Anpanman <kondratev-alesha69@mail.ru>
2021-01-23 12:44:22 +0300

    kill split

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -71,13 +71,11 @@ def caesar_breaker_brute_force(ciphertext: str, dictionary: tp.Set[str]) -> int:
     """
     best_shift = 0
     lst = []
-    grid = ciphertext.replace(".", "").split()
-    for word in grid:
-        for key in dictionary:
-            for shift in range(26):
-                plaintext = decrypt_caesar(word, shift)
-                if key == plaintext:
-                    lst.append(shift)
+    for key in dictionary:
+        for shift in range(26):
+            plaintext = decrypt_caesar(ciphertext, shift)
+            if key == plaintext:
+                lst.append(shift)
     for i in range(26):
         if best_shift < lst.count(i):
             best_shift = i
